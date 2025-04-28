5f42124382be9ddd10936f580d62971d0252f77b
yokoyamadonuts <yokoyama.shingo@donuts.ne.jp>
2025-04-28 19:14:33 +0900

    typecheck関数の実装

---
+++ typecheck.ts
@@ -0,0 +1,40 @@
+import { parse, parseArith } from "./tiny-ts-parser.ts";
+
+type Term =
+    | { tag: "true" }
+    | { tag: "false" }
+    | { tag: "if"; cond: Term; thn: Term; els: Term }
+    | { tag: "number"; n: number }
+    | { tag: "add"; left: Term; right: Term };
+
+type Type =
+    | { tag: "Boolean" }
+    | { tag: "Number" };
+
+function typecheck(t: Term): Type {
+    switch (t.tag) {
+        case "true":
+            return { tag: "Boolean" };
+        case "false":
+            return { tag: "Boolean" };
+        case "if": {
+            const condTy = typecheck(t.cond);
+            if (condTy.tag !== "Boolean") { throw "boolean expected"; }
+            const thnTy = typecheck(t.thn);
+            const elsTy = typecheck(t.els);
+            if (thnTy.tag !== elsTy.tag) { throw "then and else must have the same type"; }
+            return thnTy;
+        }
+        case "number":
+            return { tag: "Number" };
+        case "add": {
+            const leftTy = typecheck(t.left);
+            if (leftTy.tag !== "Number") { throw "number expected"; }
+            const rightTy = typecheck(t.right);
+            if (rightTy.tag !== "Number") { throw "number expected"; }
+            return { tag: "Number" };
+        }
+    }
+}
+
+console.log(typecheck(parseArith("true ? 2 : 3")));
