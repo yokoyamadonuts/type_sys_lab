614d37a790e21d9222fc2a130325b1992d576e20
yokoyamadonuts <yokoyama.shingo@donuts.ne.jp>
2025-05-06 19:47:18 +0900

    Term型の拡張

--- types/terms.ts
+++ types/terms.ts
@@ -3,8 +3,17 @@ export type Term =
     | { tag: "false" }
     | { tag: "if"; cond: Term; thn: Term; els: Term }
     | { tag: "number"; n: number }
-    | { tag: "add"; left: Term; right: Term };
+    | { tag: "add"; left: Term; right: Term }
+    | { tag: "var"; name: string }
+    | { tag: "func"; params: Param[]; body: Term }
+    | { tag: "call"; func: Term; args: Term[] }
+    | { tag: "seq"; body: Term; rest: Term }
+    | { tag: "const"; name: string; init: Term, rest: Term }
+type Param = {
+    name: string;
+    type: Type;
+}
 
 export type Type =
     | { tag: "Boolean" }
-    | { tag: "Number" }; 
+    | { tag: "Number" };
